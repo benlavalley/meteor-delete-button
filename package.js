d05d85ea1accf0f15b7a11c07321ee99157be8ed
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 02:38:52 -0400

    version bump

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "aldeed:delete-button",
-  version: "2.0.0",
+  version: "2.0.1",
   summary: "Provides a delete button UI component",
   git: "https://github.com/aldeed/meteor-delete-button.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('METEOR@1.8.1');
   api.use(['templating', 'mongo']);
 
   api.addFiles(['delete-button.html', 'delete-button.js'], 'client');
